1ba2159d6126311b8a1450acaeb864427f30e1ef
Nikhil Khandare <technoniks007@gmail.com>
2020-08-24 11:01:10 +0530

    sample SplashScreen from nikhilk

---
+++ app/src/main/java/com/farmer/krushisarathi/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.farmer.krushisarathi;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Handler;
+import android.view.View;
+
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(SplashScreen.this, MainActivity.class));
+            }
+        }, 3000);
+    }
+}
